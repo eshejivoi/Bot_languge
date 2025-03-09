f5a8d1e0c40f3a2053e675d65fb853ff152190ff
Aldement <157736734+Aldement@users.noreply.github.com>
2025-03-09 16:32:19 +0700

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,4 +1,5 @@
 import random
+import speech_recognition as sr
 
 
 test_questions = {
@@ -58,4 +59,25 @@ def track_progress(user_data, correct_answers, total_questions):
     correct = sum(1 for answer in user_data if answer in correct_answers)
     progress = (correct / total_questions) * 100
     return progress
+
+# Функция для преобразования голосового сообщения в текст
+def recognize_speech(audio_file_path):
+    recognizer = sr.Recognizer()
+
+    # Открываем аудиофайл
+    with sr.AudioFile(audio_file_path) as source:
+        audio = recognizer.record(source)  # Считываем весь файл в память
+
+    try:
+        # Используем Google Web Speech API для распознавания речи
+        recognized_text = recognizer.recognize_google(audio)
+        return recognized_text
+    except sr.UnknownValueError:
+        return "Ошибка, не удалось распознать речь"
+    except sr.RequestError:
+        return "Ошибка с интернет-соединением"
+
+# Функция для проверки произнесенной фразы
+def check_pronunciation(user_audio_text, correct_phrase):
+    return user_audio_text.strip().lower() == correct_phrase.strip().lower()
 
